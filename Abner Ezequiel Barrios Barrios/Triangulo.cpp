90821e91773122b961b18a7e25c185bfd2ac2267
Ezequiel Barrios <164549293+BarriosEzequiel800@users.noreply.github.com>
2024-03-28 16:27:06 -0600

    Create Triangulo.cpp

---
+++ Abner Ezequiel Barrios Barrios/Triangulo.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	double base, altura;
+	
+	cout << "Ingrese la longitud de la base del triangulo.";
+	cin >> base;
+	
+	cout << "Ingrese la altura del triangulo.";
+	cin >> base;
+	
+	double area= (base*altura)/2;
+	cout << "El area del triangulo es: "<< area<<endl;
+	
+	return 0;
+	
+	}
