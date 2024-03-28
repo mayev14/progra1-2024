fc73a6b2292db6d55b408ab8a7bda4bef4b91f47
Ezequiel Barrios <164549293+BarriosEzequiel800@users.noreply.github.com>
2024-03-28 16:28:03 -0600

    Create Rectangulo.cpp

---
+++ Abner Ezequiel Barrios Barrios/Rectangulo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main () {
+	double base, altura ;
+	cout <<"ingrese la longitud de la base del rectangulo:";
+	cin>> base; 
+	cout << "Ingrese la altura del rectangulo:";
+	cin>> altura;
+	double area= base*altura;
+	cout<<"el area del rectangulo es:" <<area<<endl;
+	return 0;
+	
+}
